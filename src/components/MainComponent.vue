efa16bb553b875551a3cc7d38662ecdf78eb72af
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-04-12 01:36:24 +0200

    Done

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,13 +2,13 @@
   <div class=" container-fluid ct-main ">
     
       <div v-if='albums.length > 0'  >
-        <div class="  row  align-items-scretch justify-content-evenly ct-h90  ct-card-container ">
-        <CardContent v-for='album in albums' :key='album.name +album.author' :album='album' />   
-        </div>    
-        </div>
-        <div v-else>
+          <SelectComponent :dinamicOption='optionGenre' @filterByGenre='filterSelect'/>
+          <div class="  row  align-items-scretch justify-content-evenly ct-h90  ct-card-container ">
+            <CardContent v-for='album in filteredList' :key='album.name +album.author' :album='album' />   
+          </div>    
+      </div>
+      <div v-else>
           <LoaderComponent/>
-        
       </div>
   </div>
 </template>
@@ -18,6 +18,7 @@
 import axios from 'axios';
 import CardContent from '@/components/CardContent.vue';
 import LoaderComponent from '@/components/LoaderComponent.vue';
+import SelectComponent from '@/components/SelectComponent.vue';
 
 export default {
     name:'MainComponent',
@@ -26,25 +27,54 @@ export default {
           
     data(){
       return {
-        albums:[]
+        albums:[],
+        optionGenre:[],
+        albumGenre:''
       }
     },
     props:{
       url:String
     },
-     components:{CardContent,LoaderComponent},
+    computed:{
+      filteredList(){
+        return this.albums.filter((item)=>item.genre.includes(this.albumGenre))
+      }
+    },
+     components:{CardContent,LoaderComponent,SelectComponent},
     mounted(){
       this.loadData();
+    
+      
+     
+      
+     
+      
     },
     methods:{
       loadData(){
         axios.get(this.url).then((response)=>{
           if(response.status===200){
             this.albums=response.data.response;
+            this.albums.forEach(element=>{
+	if(!this.optionGenre.includes(element.genre)){
+		this.optionGenre.push(element.genre);
+   
+	}
+});
           }
-
         })
+        
+      },
+      filterSelect(albumGenre){
+        this.albumGenre=albumGenre;
+        console.log('coap',this.albumGenre)
+        
       }
+      
+
+    
+        
+      
     }
 
 }
---
+++ src/components/SelectComponent.vue
@@ -0,0 +1,60 @@
+<template>
+<div>
+    <label for="filter">Filter By Genre</label>
+    <select name="filter" @change='selectGenre($event)'>
+        <option value="">All</option>
+        <option v-for='genre in dinamicOption' :key="genre">{{genre}}</option>
+       
+    </select>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name:'SelectComponent',
+    data(){
+        return {
+            selected:''
+        }
+    },
+    props:{dinamicOption:[]},
+    methods:{
+        selectGenre(event){
+            this.selected=event.target.value;
+            this.$emit('filterByGenre',this.selected);
+            console.log(this.selected)
+        }
+    }
+
+}
+</script>
+
+<style  lang="scss" scoped>
+@import '@/style/variables.scss';
+label{
+    color:white;
+    padding:10px;
+    border-radius:5px;
+    margin:1rem;
+   
+}
+select{
+    outline: none;
+    background-color:$secondary-color;
+    color:white;
+    border:none;
+    padding:10px;
+    border-radius:5px;
+    
+
+    &:hover{
+         box-shadow: rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.664) 0px 5px 10px;
+
+    }
+    
+   
+    
+}
+
+</style>
